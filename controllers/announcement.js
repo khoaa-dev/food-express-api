4a0ce7a291b950a3181eff351f0209e88dd9d678
nguyendinhkhoa19t1@gmail.com <nguyendinhkhoa19t1@gmail.com>
2021-12-04 21:58:36 +0700

    update function get announcement by customer id

--- controllers/announcement.js
+++ controllers/announcement.js
@@ -17,13 +17,16 @@ export const getAllAnnouncement = (req, res) => {
 export const getAnnouncementByCustomerId = (req, res) => {
     const {customerId} = req.params;
     sql.connect(config).then(pool => {
-        return pool.request().query(`select * from Announcement where customerId = ${customerId}`);
+        return pool.request().query(`select Announcement.id, nameAnnoun, imageUrl, detail, createdTime, checked, Announcement.typeId, customerId
+                                    from Announcement, AnnouncementType 
+                                    where Announcement.typeId = AnnouncementType.id and
+                                    customerId = ${customerId}`);
     })
     .then(result => {
         res.send(result.recordset);
     })
     .catch(err => {
-        res.send('error: ', err);
+        console.log(err);
         
     })
 }
