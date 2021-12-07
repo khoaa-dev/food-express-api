2f97498877fd5181af4ef9d30cc583cf62a92515
nguyendinhkhoa19t1@gmail.com <nguyendinhkhoa19t1@gmail.com>
2021-12-07 19:16:31 +0700

    Update sort announcement by time

--- controllers/announcement.js
+++ controllers/announcement.js
@@ -3,7 +3,7 @@ import config from '../config.js';
 
 export const getAllAnnouncement = (req, res) => {
     sql.connect(config).then(pool => {
-        return pool.request().query(`select * from Announcement`);
+        return pool.request().query(`select * from Announcement order by createdTime desc`);
     })
     .then(result => {
         res.send(result.recordset);
