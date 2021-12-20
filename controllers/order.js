26402b8f035d48b9dca0be020f42b7b3622d3630
nguyendinhkhoa19t1@gmail.com <nguyendinhkhoa19t1@gmail.com>
2021-12-20 22:52:53 +0700

    update order

--- controllers/order.js
+++ controllers/order.js
@@ -3,7 +3,7 @@ import config from '../config.js';
 
 export const getAllOrder = (req, res) => {
     sql.connect(config).then(pool => {
-        return pool.request().query('select * from Orders order by createdTime desc');
+        return pool.request().query('select * from Orders');
     })
     .then(result => {
         res.send(result.recordset);
